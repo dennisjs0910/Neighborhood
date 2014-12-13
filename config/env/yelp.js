cb8c4343c9ca71a63b7290983a272c78335465a6
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-12-13 14:10:38 -0800

    configuring yelp api within env, app/controller, app/routes

---
+++ config/env/yelp.js
@@ -0,0 +1,6 @@
+var yelp = require('yelp').createClient({
+  consumer_key: 'z9ZJGiqysEN3lAmY_1avUA', 
+  consumer_secret: 'my4TeKx3LvZMmb2h051vYI7Ppwo',
+  token: 'RtLkt4LO8LM4CfHlAVMcafyfiKNqjxO0',
+  token_secret: 'ooGSsZO7WmluGcpGxfzqg8DzNg0'
+});
