c0533e096f18b8f413361ca65927ac13850dd388
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2014-12-14 20:17:31 -0800

    config/env yelp.js client working, wip for getting json objects bacl

--- app/controllers/yelp.server.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _ = require('lodash');
-
-console.log(yelp);
-yelp.search({term: 'food', location: 'Montreal'}, function(error, data) {
-  console.log(error);
-  console.log(data);
-});
---
+++ config/env/yelp.js
@@ -0,0 +1,9 @@
+'use strict';
+var yelp = require('yelp').createClient({
+  consumer_key: 'z9ZJGiqysEN3lAmY_1avUA', 
+  consumer_secret: 'my4TeKx3LvZMmb2h051vYI7Ppwo',
+  token: 'dBSr4fnERdPHDGOy9BP6g0Dx9_0rsZqG',
+  token_secret: 'goHBejF9NzBIJtKgunQ16EUVlb0'
+});
+
+module.exports.yelp = yelp;
